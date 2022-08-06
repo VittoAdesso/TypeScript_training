e03fe08c5f9d43c71bf8849cdb9ba832a167e934
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-06 12:03:36 +0200

    decorators

--- ts-intro/src/ejercicios/11_decorators.ts
+++ ts-intro/src/ejercicios/11_decorators.ts
@@ -1,1 +1,23 @@
 // https://www.typescriptlang.org/docs/handbook/decorators.html#class-decorators
+
+function classDecorator<T extends { new (...args: any[]): {}}>(
+    constructor: T
+){
+    return class extends constructor {
+        newProperty = 'new one';
+        hello = 'Over';
+    };
+}
+@classDecorator
+class superClass {
+    public miPropiedad: string = 'AJDJD';
+
+    print(){
+        console.log('HELLOOO'); 
+    }
+}
+
+console.log( superClass );
+
+const myClass = new superClass();
+console.log( myClass.miPropiedad );
